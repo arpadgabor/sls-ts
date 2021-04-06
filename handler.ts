7eedbe73afcd770e6d4e17b54905b55df76a91d7
Arpad Gabor <hello@arpadgabor.com>
2021-04-06 19:24:41 +0300

    tested

--- handler.ts
+++ handler.ts
@@ -1,15 +1,44 @@
 import { Context, APIGatewayEvent, APIGatewayProxyResult } from "aws-lambda";
 
+import Knex from 'knex'
+
+const db = Knex({
+  client: 'pg',
+  connection: {
+    host: 'redacted',
+    database: 'luckydog',
+    user: 'postgres',
+    password: 'myluckydog',
+    port: 5432,
+  },
+})
+
+function toResponse(status: number, body: any): { statusCode: number, body: string } {
+  return {
+    statusCode: status,
+    body: JSON.stringify(body),
+  }
+}
+
 export async function hello(
   event: APIGatewayEvent,
   context: Context
 ): Promise<APIGatewayProxyResult> {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: "Go Serverless v2.0! Your function executed successfully!",
-      context,
-      event,
-    }),
-  };
+
+  try {
+    if(await db.schema.hasTable('dogs')) {
+      return toResponse(400, {
+        message: 'Table Exists'
+      })
+    }
+
+    const table = await db.schema.createTable('dogs', t => {
+      t.increments('id').primary()
+      t.string('name').notNullable()
+    })
+
+    return toResponse(200, { message: 'Table Dogs Created' })
+  } catch (err) {
+    return toResponse(500, { error: err })
+  }
 }
